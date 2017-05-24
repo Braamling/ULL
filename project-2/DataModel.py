bda208da33a3b41abab6a154e79029b4b229c1ea
Bram van den Akker <git@bramvandenakker.nl>
2017-05-24 23:14:19 +0200

    project part2 init

---
+++ project-2/DataModel.py
@@ -0,0 +1,34 @@
+"""
+Unsupervised Language Learning (ULL) 2017 - project 2
+
+### DataModel
+This model is a helper class for loading and using the preprocessed dataset
+
+### Authors
+Jorn Peters & Bram van den Akker
+
+"""
+
+import pickle
+import numpy as np
+
+class DataModel():
+    def __init__(self, config):
+        self.config = config
+
+        self.pairs = pickle.load(open(self.config.pairs))
+        self.id2word = pickle.load(open(self.config.id2word))
+        self.id2vector = pickle.load(open(self.config.id2vector))
+        self.id2rel = pickle.load(open(self.config.id2rel))
+
+        self.batch_pointer = 0
+
+    def get_batch(self, size=1000):
+        batch = self.pairs[self.batch_pointer: self.batch_pointer + size]
+
+        batch = [np.append(np.append(self.id2vector[verb], self.id2vector[noun]), np.asarray([rel])) for verb, noun, rel in batch]
+
+        self.batch_pointer += size
+
+        return batch
+
---
+++ project-2/main.py
@@ -0,0 +1,38 @@
+"""
+# Unsupervised Language Learning (ULL) 2017 - project 2
+
+### Main
+Main file for configuration and examples
+
+### Authors
+Jorn Peters & Bram van den Akker
+
+**Compatiable for both python2.7 and 3.0 but the pickle files are not
+  transferable**
+"""
+import logging
+
+from DataModel import DataModel
+
+logging.basicConfig(level=logging.INFO)
+
+
+class Config():
+    # Picked data model file locations
+    pairs = "storage/pairs.p"
+    id2word = "storage/id2word.p"
+    id2vector = "storage/id2vector.p"
+    id2rel = "storage/id2rel.p"
+
+
+def main():
+    config = Config()
+
+    dataModel = DataModel(config)
+
+    dataModel.get_batch(size=100000)
+
+
+
+if __name__ == '__main__':
+    main()
